47547c2fb44eb63c3a711d4d398b48650edaf9a2
abdelmasry <abdelrahman20.ibrahim@gmail.com>
2023-02-11 16:07:21 +0200

    Modified Server

--- final_project/server.py
+++ final_project/server.py
@@ -8,19 +8,21 @@ app = Flask("Web Translator")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    return translator.englishtofrench(textToTranslate)
+    result = translator.englishtofrench(textToTranslate) 
+    return result
     
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    return translator.frenchtoenglish(textToTranslate)
+    result = translator.frenchtotenglish(textToTranslate)
+    return result
 
 @app.route("/")
 def renderIndexPage():
     # Write the code to render template
-    return render_template("index.html")
+    return render_template("DEVOPs/FinalProject/xzceb-flask_eng_fr/final_project/templates/index.html")
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+if __name__=="__main__":
+    app.run(debug=True) 
